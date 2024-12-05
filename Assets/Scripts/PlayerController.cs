55104db04d5e6403217d10faec139ca252ef7322
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-12-06 03:09:01 +0530

    Player Controller Script Optimizations!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,125 +4,103 @@ using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
 public class PlayerController : MonoBehaviour {
-    Vector2 moveInput;
-    TouchingDirections touchingDirections;
+    // Movement settings
     public float walkSpeed = 5f;
     public float runSpeed = 8f;
     public float airWalkSpeed = 3f;
-    [SerializeField] private bool _isMoving = false;
-    [SerializeField] private bool _isRunning = false;
+    public float jumpImpulse = 10f;
 
-    public bool _isFacingRight = true;
+    // State variables
+    private Vector2 moveInput;
+    private bool _isFacingRight = true;
+    private bool _isMoving = false;
+    private bool _isRunning = false;
 
-    public float CurrentMoveSpeed {
-        get {
-            if (CanMove) {
-                if (IsMoving && !touchingDirections.IsOnWall) {
-                    if (touchingDirections.IsGrounded) {
-                        if (IsRunning) {
-                            return runSpeed;
-                        } else {
-                            return walkSpeed;
-                        }
-                    } else {
-                        return airWalkSpeed;
-                    }
-                } else {
-                        return 0;
-                    }
-            } else {
-                return 0;
-            }
-            
-        }
-    }
+    // Cached components
+    private Rigidbody2D rb;
+    private Animator animator;
+    private TouchingDirections touchingDirections;
 
-    public bool IsMoving {
-        get {
-            return _isMoving;
-        } 
-        private set {
+    // Properties for state management
+    private float CurrentMoveSpeed => CanMove 
+        ? (IsMoving && !touchingDirections.IsOnWall 
+            ? (touchingDirections.IsGrounded ? (IsRunning ? runSpeed : walkSpeed) : airWalkSpeed) 
+            : 0)
+        : 0;
+
+    private bool IsMoving {
+        get => _isMoving;
+        set {
             _isMoving = value;
             animator.SetBool(AnimationStrings.isMoving, value);
         }
     }
 
-    public bool IsRunning {
-        get {
-            return _isRunning;
-        } 
-        private set {
+    private bool IsRunning {
+        get => _isRunning;
+        set {
             _isRunning = value;
             animator.SetBool(AnimationStrings.isRunning, value);
         }
     }
 
-    public bool IsFacingRight { 
-        get {
-            return _isFacingRight;
-        } 
-        private set{
-            if(_isFacingRight != value) {
-                transform.localScale *= new Vector2(-1, 1);
+    private bool IsFacingRight {
+        get => _isFacingRight;
+        set {
+            if (_isFacingRight != value) {
+                transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
             }
             _isFacingRight = value;
         }
     }
 
-    public bool CanMove {
-        get {
-            return animator.GetBool(AnimationStrings.canMove);
-        }
-    }
-
-    Rigidbody2D rb;
-    Animator animator;
-    public float jumpImpulse = 10f;
+    private bool CanMove => animator.GetBool(AnimationStrings.canMove);
 
     private void Awake() {
+        // Cache necessary components
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         touchingDirections = GetComponent<TouchingDirections>();
     }
 
     private void FixedUpdate() {
+        // Update player's horizontal velocity
         rb.linearVelocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.linearVelocity.y);
         animator.SetFloat(AnimationStrings.velocityY, rb.linearVelocityY);
     }
 
     public void OnMove(InputAction.CallbackContext context) {
+        // Handle movement input
         moveInput = context.ReadValue<Vector2>();
-
         IsMoving = moveInput != Vector2.zero;
-
-        SetFacingDirection(moveInput);
+        SetFacingDirection(moveInput.x);
     }
 
     public void OnRun(InputAction.CallbackContext context) {
-        if (context.started) {
-            IsRunning = true;
-        } else if (context.canceled) {
-            IsRunning = false;
-        }
+        // Handle run toggle
+        IsRunning = context.started;
     }
 
     public void OnJump(InputAction.CallbackContext context) {
+        // Handle jump input
         if (context.started && touchingDirections.IsGrounded) {
             animator.SetTrigger(AnimationStrings.jumpTrigger);
             rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpImpulse);
         }
     }
 
-    public void onAttack(InputAction.CallbackContext context) {
+    public void OnAttack(InputAction.CallbackContext context) {
+        // Trigger attack animation
         if (context.started) {
             animator.SetTrigger(AnimationStrings.attackTrigger);
         }
     }
 
-    private void SetFacingDirection(Vector2 moveInput) {
-        if (moveInput.x > 0 && !IsFacingRight) {
+    private void SetFacingDirection(float horizontalInput) {
+        // Flip character based on movement direction
+        if (horizontalInput > 0 && !IsFacingRight) {
             IsFacingRight = true;
-        } else if (moveInput.x < 0 && IsFacingRight) {
+        } else if (horizontalInput < 0 && IsFacingRight) {
             IsFacingRight = false;
         }
     }
