80d4fd8f95019596444c03e277b02a46a721b29b
Prathmesh Ojha <prathmeshojha2307@gmail.com>
2024-12-06 03:41:30 +0530

    Touching Directions Script Optimizations!

--- Assets/Scripts/TouchingDirections.cs
+++ Assets/Scripts/TouchingDirections.cs
@@ -1,62 +1,63 @@
 using UnityEngine;
 
-public class TouchingDirections : MonoBehaviour
-{
+public class TouchingDirections : MonoBehaviour {
+    // Settings for raycast detection
     public ContactFilter2D castFilter;
     public float groundDistance = 0.05f;
     public float wallDistance = 0.2f;
     public float ceilingDistance = 0.05f;
-    CapsuleCollider2D touchingCollider;
-    Animator animator;
-    readonly RaycastHit2D[] groundHits = new RaycastHit2D[5];
-    readonly RaycastHit2D[] wallHits = new RaycastHit2D[5];
-    readonly RaycastHit2D[] ceilingHits = new RaycastHit2D[5];
 
-    [SerializeField] private bool _isGrounded;
+    // Cached components
+    private CapsuleCollider2D touchingCollider;
+    private Animator animator;
 
-    public bool IsGrounded { 
-        get {
-            return _isGrounded;
-        } 
+    // State variables
+    private bool _isGrounded;
+    private bool _isOnWall;
+    private bool _isOnCeiling;
+
+    // Properties for state access
+    public bool IsGrounded {
+        get => _isGrounded;
         private set {
             _isGrounded = value;
             animator.SetBool(AnimationStrings.isGrounded, value);
         }
     }
-    
-    [SerializeField] private bool _isOnWall;
 
-    public bool IsOnWall { 
-        get {
-            return _isOnWall;
-        } 
+    public bool IsOnWall {
+        get => _isOnWall;
         private set {
             _isOnWall = value;
             animator.SetBool(AnimationStrings.isOnWall, value);
         }
     }
 
-    [SerializeField] private bool _isOnCeiling;
-    private Vector2 WallCheckDirection => gameObject.transform.localScale.x > 0 ? Vector2.right : Vector2.left;
-
-    public bool IsOnCeiling { 
-        get {
-            return _isOnCeiling;
-        } 
+    public bool IsOnCeiling {
+        get => _isOnCeiling;
         private set {
             _isOnCeiling = value;
             animator.SetBool(AnimationStrings.isOnCeiling, value);
         }
     }
 
-    private void Awake () {
+    private Vector2 WallCheckDirection => transform.localScale.x > 0 ? Vector2.right : Vector2.left;
+
+    private void Awake() {
+        // Cache necessary components
         touchingCollider = GetComponent<CapsuleCollider2D>();
         animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate() {
-        IsGrounded = touchingCollider.Cast(Vector2.down, castFilter, groundHits, groundDistance) > 0;
-        IsOnWall = touchingCollider.Cast(WallCheckDirection, castFilter, wallHits, wallDistance) > 0;
-        IsOnCeiling = touchingCollider.Cast(Vector2.up, castFilter, ceilingHits, ceilingDistance) > 0;
+    private void FixedUpdate() {
+        // Check for ground, wall, and ceiling collisions
+        IsGrounded = DetectCollision(Vector2.down, groundDistance);
+        IsOnWall = DetectCollision(WallCheckDirection, wallDistance);
+        IsOnCeiling = DetectCollision(Vector2.up, ceilingDistance);
+    }
+
+    private bool DetectCollision(Vector2 direction, float distance) {
+        // Perform raycast in the specified direction
+        return touchingCollider.Cast(direction, castFilter, new RaycastHit2D[5], distance) > 0;
     }
 }
